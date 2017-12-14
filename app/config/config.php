606d7bc010e5464a1d44739e2653b626fdd002df
leon1757tw <leon1757tw@gmail.com>
2017-12-14 11:11:18 +0800

    Modified config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,15 @@
 <?PHP
     define('DB_SERVER', '140.123.175.101');
-    define('DB_DATABASE', 'team08');
-    define('DB_USERNAME', 'team08');
+    define('DB_NAME', 'team08');
+    define('DB_ACCOUNT', 'team08');
     define('DB_PASSWORD', 'hotdog');
     
-    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-    
-    if(!$db){
-        die("Connection Failed: " .mysqli_error());
+    try{
+        $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8', DB_ACCOUNT, DB_PASSWORD);
+        echo "Connection Successful";
+    } catch ( PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
     }
-    mysqli_set_charset($db, "utf8");
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
