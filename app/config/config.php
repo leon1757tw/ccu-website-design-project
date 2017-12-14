318b36809c341a4cdcf153d5e46b355cbf8bfd93
leon1757tw <leon1757tw@gmail.com>
2017-12-15 02:21:48 +0800

    Modified file
    modified -> config.php, account.php
    deleted -> logout.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,8 @@
 <?PHP
-    define('DB_SERVER', '140.123.175.101');
-    define('DB_NAME', 'team08');
-    define('DB_ACCOUNT', 'team08');
-    define('DB_PASSWORD', 'hotdog');
-    
-    try{
-        $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME.';charset=utf8', DB_ACCOUNT, DB_PASSWORD);
-        echo "Connection Successful";
-    } catch ( PDOException $e){
-        echo "Connection failed: " . $e->getMessage();
-    }
-    
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+define('DB_SERVER', '140.123.175.101');
+define('DB_NAME', 'team08');
+define('DB_ACCOUNT', 'team08');
+define('DB_PASSWORD', 'hotdog');
+ 
 ?>
--- app/controllers/account.php
+++ app/controllers/account.php
@@ -1,38 +1,30 @@
 <?PHP
-    require_once('../config/config.php');
+    require_once('../database.php');
+
+    $db = new DBController();
 
     extract($_POST);
     
     if(isset($login)){
-        try{
-            $stmt = $db->prepare("SELECT * FROM account WHERE username = ? AND password = ?");
-            $stmt->execute(array("123", "456"));
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e){
-            echo "Database Error:" . $e->getMessage();
-        }
-        
+        $result = $db->queryDB("SELECT * FROM account WHERE username = ? AND password = ?", 
+                                array($username, $password));
         session_start();
-        if($stmt->execute() && !empty($result)){
+        if(!empty($result)){
             $_SESSION["login_user"] = $username;
-            echo "Login Successful";
             header("Location:");
+            echo "Login Successful";
         }
     }
     else if(isset($register)){
-        try{
-            $stmt = $db->prepare("INSERT INTO account VALUES (?, ?, ?, ?)");
-            $stmt->execute(array($username, $password, $email, $phone));
-        } catch (PDOException $e){
-            echo "Database Error:" . $e->getMessage();
-        }
-
-        if($stmt->execute()){
+        $result = $db->executeDB("INSERT INTO account VALUES (?, ?, ?, ?)", 
+                                array($username, $password, $email, $phone));
+        if($result){
+            header("Location:");
             echo "Sign Up Successful";
-        }
-        header("Location:");
+        }      
     }
     else{
         header("Location:");
     }
+
 ?>
