e3e87080644d4e2357aa51f493cd2746be88cee1
CZJ <followmytype@gmail.com>
2017-12-28 20:19:45 +0800

    add UPDATE and DELETE function 	modified:   app/model/ticket.php

--- app/model/ticket.php
+++ app/model/ticket.php
@@ -2,7 +2,7 @@
 require_once("../model/database.php");
 
 /**
-* 
+*大致上都打好了拉，最後改個資料表的名稱以及每個欄位的名稱就降 
 */
 class Ticket
 {
@@ -33,17 +33,30 @@ class Ticket
 	}
 
 	public function takeTicket(){
-		$result = $this->db->executeDB_selc("SELECT * FROM ticket");
-		$row = $result->fetchAll(PDO::FETCH_ASSOC);
-		return $row;
-		//$row = $result->fetchAll();
-		/*while(){
-			print_r($row);
-		}*/
-		/*foreach ($row as $key => $value) {
-			echo "$key: $value<br>";
-		}*/
-		//print_r ($row = $result->fetchAll());
+		$result = $this->db->queryDB("SELECT * FROM ticket",array());
+		return $result;
+	}
+
+	public function updateTicket($ticketName,$ticketAmount,$ticketPrice,$ticketInfo,$ticketId){
+		$this->ticketId = $ticketId;
+		$this->ticketName = $ticketName;
+		$this->ticketAmount = $ticketAmount;
+		$this->ticketPrice = $ticketPrice;
+		$this->ticketInfo = $ticketInfo;
+
+		$result = $this->db->executeDB("UPDATE ticket SET t_name=?, t_amount=?, t_price=?, t_info=?
+WHERE t_id=$this->ticketId;",array($this->ticketName,$this->ticketAmount,$this->ticketPrice,$this->ticketInfo));
+
+		return $result;
+
+	}
+
+	public function deleteTicket($ticketId){
+		$this->ticketId = $ticketId;
+
+		$result = $this->db->executeDB("DELETE FROM ticket WHERE t_id=?",array($this->ticketId));
+
+		return $result;
 	}
 	
 }
